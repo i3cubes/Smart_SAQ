bf9d67ceec9d8e508ba4bbc5117ac68d77b1e788
Tharanga <anuradha.spta@gmail.com>
2021-01-05 16:27:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ include("inc/header.php");
 <!-- possible classes: minified, no-right-panel, fixed-ribbon, fixed-header, fixed-width-->
 
 
-<div id="main" role="main" style="margin-left:0px;background: url(img/bg.jpg) #fff;">
+<div id="main" role="main" style="margin-left:0px;">
 
 	<!-- MAIN CONTENT -->
 	<div id="content" class="container">
