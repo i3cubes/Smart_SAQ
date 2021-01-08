ee364b3e7083f7872b62f9020ba568e45c9f3f04
i3cubes <47766255+i3cubes@users.noreply.github.com>
2021-01-08 12:07:07 +0530

    Update cls_site.php

--- class/cls_site.php
+++ class/cls_site.php
@@ -615,7 +615,7 @@ class site {
         $tecnologies=array();
         $str="SELECT * FROM saq_site_technical as t1 left join saq_technical as t2 on t1.saq_technical_id=t2.id "
                 . "WHERE t1.saq_sites_id='$this->id' AND t1.available='Y'";
-        print $str;
+        //print $str;
         $res= dbQuery($str);
         while ($row = dbFetchAssoc($res)) {
             array_push($tecnologies,$row['technology']);
