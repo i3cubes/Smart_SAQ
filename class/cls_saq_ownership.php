df90101c168da3f1a759fddbad910b2df9da1db4
Tharanga <anuradha.spta@gmail.com>
2021-03-19 17:42:37 +0530

    Update cls_saq_ownership.php

--- class/cls_saq_ownership.php
+++ class/cls_saq_ownership.php
@@ -26,7 +26,7 @@ class saq_site_ownership {
         $string = "INSERT INTO `$this->table_name` (`ownership`) VALUES (" . getStringFormatted($this->ownership) . ");";
         $result = dbQuery($string);
         if ($result) {
-            return true;
+            return dbInsertId();
         } else {
             return false;
         }
@@ -52,6 +52,56 @@ class saq_site_ownership {
             array_push($array, $saq_ownership_obj);
         }
         return $array;
+    }
+    function search($ownership,$status,$limitcount,$orderby,$order,$likeType){
+        //liketype =,%A%, A%,%B
+        $array_sql = array();
+        if($ownership !=""){
+            if($likeType=="="){
+                array_push($array_sql, "ownership = '$ownership'");
+            }else if($likeType =="%A%"){
+                array_push($array_sql, "ownership LIKE '%$ownership%'");
+            }else if($likeType =="A%"){
+                array_push($array_sql, "ownership LIKE '$ownership%'");
+            }else if($likeType=="%A"){
+                array_push($array_sql, "ownership LIKE '%$ownership'");
+            }else {
+                array_push($array_sql, "ownership LIKE '$ownership'");
+            }
+            
+        }
+        if($status !=""){
+            
+        }
+        $LIMIT = "";
+        if($limitcount !=""){
+            $LIMIT = "LIMIT ".$limitcount;
+        }
+        $ORDERBY="";
+        if($orderby !=""){
+            if($order =="DESC"){
+                $order = "DESC";
+            }else {
+                $order = "ASC";
+            }
+            $ORDERBY = " ORDER BY ".$orderby." ".$order;
+            
+        }
+        
+        if(count($array_sql)>0){
+            $WHERE = " WHERE ". implode(" AND ", $array_sql);
+        }
+        $str = "SELECT * FROM $this->table_name $WHERE $ORDERBY $LIMIT";
+        //print $str;
+        $result = dbQuery($str);
+        $array= array();
+        while ($row = dbFetchAssoc($result)) {
+            $saq_ownership_obj = new saq_site_ownership($row['id']);
+            $saq_ownership_obj->getData();
+            array_push($array, $saq_ownership_obj);
+        }
+        return $array;
+        
     }
 
 }
