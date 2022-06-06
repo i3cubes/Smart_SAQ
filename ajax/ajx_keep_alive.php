924d486b36577c4fa64e473c531686c4968df577
Male94 <39874531+Male94@users.noreply.github.com>
2022-06-06 13:40:40 +0530

    mw

--- ajax/ajx_keep_alive.php
+++ ajax/ajx_keep_alive.php
@@ -11,40 +11,45 @@ require_once('../vendor/autoload.php');
 
 include_once '../class/constants.php';
 
-if (!preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
-    header('HTTP/1.0 400 Bad Request');
-    echo 'Token not found in request';
-    exit;
-}
-$jwt = $matches[1];
-if (!$jwt) {
-    // No token was able to be extracted from the authorization header
-    header('HTTP/1.0 400 Bad Request');
-    exit;
-}
-
-$secretKey = constants::$secretKey;
-
-if ($jwt != 'undefined') {
-    try {
-        $token = JWT::decode($jwt, $secretKey, ['HS512']);
-    } catch (\Firebase\JWT\ExpiredException $e) {
-        echo json_encode(array('msg' => 'Session expired!!!', 'result' => 1));
-        session_destroy();
-        exit();
+if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+    if (!preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
+        header('HTTP/1.0 400 Bad Request');
+        echo 'Token not found in request';
+        exit;
     }
-}
+    $jwt = $matches[1];
+    if (!$jwt) {
+        // No token was able to be extracted from the authorization header
+        header('HTTP/1.0 400 Bad Request');
+        exit;
+    }
+
+    $secretKey = constants::$secretKey;
+
+    if ($jwt != 'undefined') {
+        try {
+            $token = JWT::decode($jwt, $secretKey, ['HS512']);
+        } catch (\Firebase\JWT\ExpiredException $e) {
+            echo json_encode(array('msg' => 'Session expired!!!', 'result' => 1));
+            session_destroy();
+            exit();
+        }
+    }
 
 
-$now = new DateTimeImmutable();
-$serverName = constants::$serverName;
+    $now = new DateTimeImmutable();
+    $serverName = constants::$serverName;
 
-if ($token->iss !== $serverName ||
-        $token->nbf > $now->getTimestamp() ||
-        $token->exp < $now->getTimestamp()) {
+    if ($token->iss !== $serverName ||
+            $token->nbf > $now->getTimestamp() ||
+            $token->exp < $now->getTimestamp()) {
+        session_destroy();
+        echo json_encode(array('msg' => 'Session expired!!!', 'result' => 1));
+    } else {
+        echo json_encode(array('msg' => 'Session alive!!!', 'result' => 0));
+    }
+} else {
     session_destroy();
     echo json_encode(array('msg' => 'Session expired!!!', 'result' => 1));
-} else {
-    echo json_encode(array('msg' => 'Session alive!!!', 'result' => 0));
 }
 ?>
--- ajax/ajx_user.php
+++ ajax/ajx_user.php
@@ -17,36 +17,41 @@ include_once '../class/constants.php';
 //var_dump($_SERVER['HTTP_AUTHORIZATION']);
 $option = $_POST['option'];
 if ($option != 'LOGIN') {
-    if (!preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
-        header('HTTP/1.0 400 Bad Request');
-        echo 'Token not found in request';
-        exit;
-    }
-    $jwt = $matches[1];
-    if (!$jwt) {
-        // No token was able to be extracted from the authorization header
-        header('HTTP/1.0 400 Bad Request');
-        exit;
-    }
-
-    $secretKey = constants::$secretKey;
-    if ($jwt != 'undefined') {
-        try {
-            $token = JWT::decode($jwt, $secretKey, ['HS512']);
-        } catch (\Firebase\JWT\ExpiredException $e) {
-            echo json_encode(array('msg' => 'Session expired!!!', 'result' => 1));
-            session_destroy();
-            exit();
+    if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+        if (!preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
+            header('HTTP/1.0 400 Bad Request');
+            echo 'Token not found in request';
+            exit;
         }
-    }
-    $now = new DateTimeImmutable();
-    $serverName = constants::$serverName;
+        $jwt = $matches[1];
+        if (!$jwt) {
+            // No token was able to be extracted from the authorization header
+            header('HTTP/1.0 400 Bad Request');
+            exit;
+        }
 
-    if ($token->iss !== $serverName ||
-            $token->nbf > $now->getTimestamp() ||
-            $token->exp < $now->getTimestamp()) {
-        header('HTTP/1.1 401 Unauthorized');
-        exit;
+        $secretKey = constants::$secretKey;
+        if ($jwt != 'undefined') {
+            try {
+                $token = JWT::decode($jwt, $secretKey, ['HS512']);
+            } catch (\Firebase\JWT\ExpiredException $e) {
+                echo json_encode(array('msg' => 'Session expired!!!', 'result' => 1));
+                session_destroy();
+                exit();
+            }
+        }
+        $now = new DateTimeImmutable();
+        $serverName = constants::$serverName;
+
+        if ($token->iss !== $serverName ||
+                $token->nbf > $now->getTimestamp() ||
+                $token->exp < $now->getTimestamp()) {
+            header('HTTP/1.1 401 Unauthorized');
+            exit;
+        }
+    } else {
+        echo json_encode(array('msg' => 'Session expired!!!', 'result' => 1));
+        session_destroy();
     }
 }
 
